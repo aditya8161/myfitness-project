d96d294b1023e6aa387815406c65c857df083c60
aditya8161 <adityasutar18092003@gmail.com>
2025-12-30 18:33:30 +0530

    Create MyFitness Project

---
+++ src/main/java/com/project/MyFitness/MyFitnessApplication.java
@@ -0,0 +1,13 @@
+package com.project.MyFitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFitnessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/MyFitness/MyFitnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.MyFitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFitnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
