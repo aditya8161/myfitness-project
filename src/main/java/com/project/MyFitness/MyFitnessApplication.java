471e684e292c43a7bfab824dfa586e22bd8bd1b0
aditya8161 <adityasutar18092003@gmail.com>
2026-01-06 19:18:04 +0530

    Add Essential Config DB

--- src/main/java/com/project/MyFitness/MyFitnessApplication.java
+++ src/main/java/com/project/MyFitness/MyFitnessApplication.java
@@ -4,10 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyFitnessApplication {
+public class MyFitnessApplication
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(MyFitnessApplication.class, args);
 	}
+
+	/*
+	* git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/aditya8161/myfitness-project.git
+git push -u origin main
+* */
 
 }
---
+++ src/main/java/com/project/MyFitness/controller/MyUser.java
@@ -0,0 +1,17 @@
+package com.project.MyFitness.controller;
+
+import lombok.Data;
+
+@Data
+public class MyUser
+{
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String city;
+    private String contact;
+    private String address;
+
+    
+}
